ec0ffb3c3738fb2b462ca8097982cdd68625ddf1
kennojc <kennojc@gmail.com>
2020-10-11 16:34:57 -0300

    Added Smooth Scrolling

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,11 +2,31 @@ $(function () {
       $('[data-toggle="tooltip"]').tooltip()
         });
 
+
 $(function() {
       $("#changethewords").changeWords({
         time: 3000,
         animate: "fadeInLeft",
         selector: "span",
         repeat:true
+      });
+    });
+
+    $(document).ready(function(){
+      $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+          event.preventDefault();
+
+
+          var hash = this.hash;
+
+          $('html, body').animate({
+            scrollTop: $(hash).offset().top 
+          }, 800, function(){
+
+            window.location.hash = hash;
+          });
+        }
       });
     });
